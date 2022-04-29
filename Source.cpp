102978dcdfcfcfac02f99829fbcd180a34db07f0
Bump50 <104525947+Bump50@users.noreply.github.com>
2022-04-29 20:47:38 +0100

    V 1.0

---
+++ Source.cpp
@@ -0,0 +1,30 @@
+#include <functional>
+using namespace std;
+
+
+double integrator(function<double(double)> func, double x1, double x2, int increments)
+{
+	double ans = 0.0, delta=(x2-x1)/((double) increments);
+
+	for (int i = 0; i <increments; i++) {
+
+		ans += (func(x1) + func(x1 + delta)) * delta / 2; //= func(x1) * delta + ((func(x1 + delta) - func(x1)) * delta) / 2;
+		x1 += delta;
+	}
+
+	return ans;
+	
+
+}
+
+double func(double x) {
+	return x*x+7*x+3;
+}
+#include <iostream>
+int main() {
+
+	double a = integrator(func, -1.123, 1.0, 5000);
+	cout << a;
+	cin.get();
+
+}
